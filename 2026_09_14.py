c56a96c024f48c903ac31c6c2f090e678b1bfe51
veraellaaaa <101644805+unkn0wn-nw0nknu@users.noreply.github.com>
2026-09-14 15:10:23 +0200

    Add circle circumference and area calculations

--- 2026_09_14.py
+++ 2026_09_14.py
@@ -14,3 +14,10 @@ mennyit_euroban = int(input("Mennyi eurót akarsz forintra váltani (pl. 100)? "
 forint = int(mennyit_euroban * euro) #pl. 100*365,86 ft
 print(f"{mennyit_euroban} euró átváltva forintra az {forint} Ft")
 
+#6. feladat
+
+r_erteke = int(input("Add meg a kör sugarát: "))
+print(f"A kör kerülete: {r_erteke * 3.14} cm")
+kor_terulete = 3.14*r_erteke**2
+kor_terulete = 3.14*r_erteke*r_erteke
+print(f"A kör terulete: {kor_terulete} cm")
