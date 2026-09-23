8c352e593ed0666fa84ed5aebf2850467bf67cbf
veraellaaaa <101644805+unkn0wn-nw0nknu@users.noreply.github.com>
2026-09-23 14:40:46 +0200

    Create 2026_09_23.py

---
+++ 2026_09_23.py
@@ -0,0 +1,13 @@
+barataim = ["A", "B", "C"]
+#egy tömb elemszáma 0-tól a végtelenig lehet
+print(barataim[-1])
+print(barataim[0]) #sorszámozás 0-tól kezdődik, output: E
+print(barataim[1])
+print(barataim[2])
+#print(barataim[3]) #List item out of range,a tömbnél többedik elemet akarjuk = hiba
+
+ures_tomb = []
+print(ures_tomb)
+
+#tömbök kiiratása
+print(barataim)
