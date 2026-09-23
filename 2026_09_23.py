05c87601926ab6c73b5694c9f88ea5f8e569a050
veraellaaaa <101644805+unkn0wn-nw0nknu@users.noreply.github.com>
2026-09-23 14:48:47 +0200

    Create list of favorite foods and print count
    Added a list of favorite foods and printed its length.

--- 2026_09_23.py
+++ 2026_09_23.py
@@ -11,3 +11,8 @@ print(ures_tomb)
 
 #tömbök kiiratása
 print(barataim)
+
+# Feladat: Hozz létre egy tömnöt a kedvenc videójátékok/sorozatok/hobbi dolgokról 4-6 elem
+
+kedvenceim = ["Kakaós palacsinta", "Rizskoch", "Trappista sajt", "Csokis keksz", "Sztracsatella fagyi"]
+print(f"Nekem {len(kedvenceim)} kedvenc kajám van.") #len (length) kiirja mennyi elem van egy tömbben.
